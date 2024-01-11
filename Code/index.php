566a8eb23d7252eb6c5180c1f14d4919442ae6c1
TodeschiniPaolo <101709345+TodeschiniPaolo@users.noreply.github.com>
2024-01-11 09:43:35 +0000

    k

--- Code/index.php
+++ Code/index.php
@@ -4,6 +4,9 @@
 
     // Check if the user is logged in, if not then redirect him to login page
     if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+        header("location: login.php");
+        exit;
+    }else{
         header("location: home.php");
         exit;
     }
